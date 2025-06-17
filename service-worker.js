a089563eefdabe93b755beb13b644b6474655655
Roman Rodomansky <itspoma@gmail.com>
2025-06-17 18:01:33 +0200

    feat: implement service worker and cache busting with version tracking

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+// Service Worker for instant-redirect
+// Version: 1.0.0
+
+const CACHE_NAME = 'instant-redirect-cache-v1';
+
+// Install event - cache the essential files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html'
+        ]);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event - network first, then cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Clone the response
+        const responseToCache = response.clone();
+        
+        // Open the cache and store the new response
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+        
+        return response;
+      })
+      .catch(() => {
+        // If network fetch fails, try to return from cache
+        return caches.match(event.request);
+      })
+  );
+});
---
+++ version.js
@@ -0,0 +1,3 @@
+// This file is used for cache busting
+// Update the version number whenever you make changes to the site
+window.APP_VERSION = '1.0.0_' + new Date().getTime();
